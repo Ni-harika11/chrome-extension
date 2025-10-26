d8628950e5fba97d875e814780af0f6b44490796
niharika <malikniharika048@gamil.com>
2025-10-26 21:55:36 +0530

    Voice to Text

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import VoiceRecorder from './VoiceRecorder';
+
+function App() {
+  return (
+    <div className="App w-96 p-4">
+      
+      <VoiceRecorder/>
+    </div>
+  );
+}
+
+export default App;
